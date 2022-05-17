972fde42c1fa4a2eb3cdcbd95dd127257417c8d2
Faizaan Al Faisal <101617018+FaizaanAlFaisal@users.noreply.github.com>
2022-05-17 10:21:21 +0500

    Added Template Message Functionality
    Users can now create "Template Messages". For example, a user may have a case where they like to switch between several messages, they can simply add template messages and switch back and forth with a simple click.

--- src/webpage/public/script.js
+++ src/webpage/public/script.js
@@ -4,22 +4,23 @@ $(function(){
     const messageRef = dbRef.ref('displayData');
     const templateRef = dbRef.ref('templates');
 
-    var count = 0;
+    var templateCount = 0; //keeps track of number of templates that already exist
 
-    templateRef.child('/entry' + count).on("child_added", function(snap){
-        
-        $('.templatesTable tbody').append(appendToTable(snap.val));
+    templateRef.on("child_added", function(snap){
+        $('.templatesTable tbody').append(appendToTable(snap.val()));
     });
 
     function appendToTable(info) {
-        var tableRow = '<tr id="' + count + '"><td id="msg"> ' + info.message + '</td>';
-        tableRow += '<td id="spd"> ' + info.scrollSpeed + '</td>';
-        tableRow += '<td id="ints"> ' + info.intensity + '</td>';
-        tableRow += '<td><a id="' + count + '" href="javascript:writeToDisplay();">Set Message</a></td></tr>';
-    }
+        // console.log("Adding template: " + info);
 
-    function writeToDisplay() {
-        console.log('test of function')
+        var tableRow = '<tr id="' + templateCount + '"><td class="msg"> ' + info.message + '</td>';
+        tableRow += '<td class="spd"> ' + info.scrollSpeed + '</td>';
+        tableRow += '<td class="ints"> ' + info.intensity + '</td>';
+        tableRow += '<td><button id="' + templateCount + '" class="btn btn-primary p-1 m-1 useTemplate">Set Message</button></td></tr>';
+
+        templateCount++; //increment counter of number of templates
+
+        return tableRow;
     }
 
     $('#submitBtn').on("click", function (event) {
@@ -38,13 +39,36 @@ $(function(){
     $('#templateBtn').on("click", function (event) {
         event.preventDefault(); //prevent form from submitting, stays on same page
 
-        templateRef.child('/entry' + count++).set({
+        templateRef.child('/entry' + templateCount).set({
             message: $('#message').val(),
             scrollSpeed: parseInt($('#scrollSpeed').val()),
             intensity: parseInt($('#intensity').val())    
         });
         
         document.getElementById('messageForm').reset(); //clear all information on form
+    });
+
+    //since elements are appended after the fact, use DOM to check for them, instead of directly
+    $(document).on("click", "button.useTemplate", function (event) { //when the "set message" link is clicked
+        event.preventDefault(); //prevent click from redirecting page
+
+        console.log("click acknowledged: " + event.target.id);
+
+        var rowID = event.target.id; //fetch id of anchor clicked, this id matches table row id
+        var templateMessage = $('tr#' + rowID + " td.msg").text(); //using row id, get message, scroll speed, intensity
+        var templateScrollSpeed = $('tr#' + rowID + " td.spd").text();
+        var templateIntensity = $('tr#' + rowID + " td.ints").text();
+
+        console.log("Template: " + templateMessage + ", " + templateScrollSpeed + ", " + templateIntensity);
+
+        messageRef.remove(); //remove the previously displayed message form LED matrix
+
+        messageRef.set({ //set() sends json data to firebase rt-db for reading by esp32
+            message: templateMessage,
+            scrollSpeed: parseInt(templateScrollSpeed),
+            intensity: parseInt(templateIntensity),
+            isBeingDisplayed: false 
+        });
     });
 
 });
