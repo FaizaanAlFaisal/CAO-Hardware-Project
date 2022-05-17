f9dc90c8f9312bbf82b13c4794ffde75c82412a4
Faizaan Al Faisal <101617018+FaizaanAlFaisal@users.noreply.github.com>
2022-05-17 11:36:15 +0500

    Updated to Add Ease of Usage
    The script-side functionality had to be changed slightly for ease-of-use changes in input-form. Done with key-value pairs that matches each word-based option to preset numeric values, simplifying the conversion process.

--- src/webpage/public/script.js
+++ src/webpage/public/script.js
@@ -6,16 +6,25 @@ $(function(){
 
     var templateCount = 0; //keeps track of number of templates that already exist
 
+    //helps usability for new-users, easy to understand simple words instead of ranges of values
+    var intensities = {"Low":2, "Medium":6, "High":10}; 
+    var speeds = {"Slow":500, "Moderate":250, "Fast":150}; 
+    //key-value pairs make conversion simple in coding
+
     templateRef.on("child_added", function(snap){
         $('.templatesTable tbody').append(appendToTable(snap.val()));
     });
 
     function appendToTable(info) {
         // console.log("Adding template: " + info);
+        var spd = ""; var ints = "";
+        if (info.scrollSpeed == 500) spd = "Slow"; else if (info.scrollSpeed == 250) spd = "Moderate"; else spd = "Fast";
+        if (info.intensity == 2) ints = "Low"; else if (info.intensity == 6) ints = "Medium"; else ints = "High"; 
 
-        var tableRow = '<tr id="' + templateCount + '"><td class="msg"> ' + info.message + '</td>';
-        tableRow += '<td class="spd"> ' + info.scrollSpeed + '</td>';
-        tableRow += '<td class="ints"> ' + info.intensity + '</td>';
+
+        var tableRow = '<tr id="' + templateCount + '"><td class="msg">' + info.message + '</td>';
+        tableRow += '<td class="spd">' + spd + '</td>';
+        tableRow += '<td class="ints">' + ints + '</td>';
         tableRow += '<td><button id="' + templateCount + '" class="btn btn-primary p-1 m-1 useTemplate">Set Message</button></td></tr>';
 
         templateCount++; //increment counter of number of templates
@@ -29,8 +38,8 @@ $(function(){
         messageRef.remove();
         messageRef.set({ //set() sends json data to firebase rt-db for reading by esp32
             message: $('#message').val(),
-            scrollSpeed: parseInt($('#scrollSpeed').val()),
-            intensity: parseInt($('#intensity').val()),
+            scrollSpeed: speeds[$('#scrollSpeed').val()],
+            intensity: intensities[$('#intensity').val()],
             isBeingDisplayed: false 
         });
         document.getElementById('messageForm').reset(); //clear all information on form
@@ -41,8 +50,8 @@ $(function(){
 
         templateRef.child('/entry' + templateCount).set({
             message: $('#message').val(),
-            scrollSpeed: parseInt($('#scrollSpeed').val()),
-            intensity: parseInt($('#intensity').val())    
+            scrollSpeed: speeds[$('#scrollSpeed').val()],
+            intensity: intensities[$('#intensity').val()]   
         });
         
         document.getElementById('messageForm').reset(); //clear all information on form
@@ -52,8 +61,6 @@ $(function(){
     $(document).on("click", "button.useTemplate", function (event) { //when the "set message" link is clicked
         event.preventDefault(); //prevent click from redirecting page
 
-        console.log("click acknowledged: " + event.target.id);
-
         var rowID = event.target.id; //fetch id of anchor clicked, this id matches table row id
         var templateMessage = $('tr#' + rowID + " td.msg").text(); //using row id, get message, scroll speed, intensity
         var templateScrollSpeed = $('tr#' + rowID + " td.spd").text();
@@ -62,11 +69,10 @@ $(function(){
         console.log("Template: " + templateMessage + ", " + templateScrollSpeed + ", " + templateIntensity);
 
         messageRef.remove(); //remove the previously displayed message form LED matrix
-
         messageRef.set({ //set() sends json data to firebase rt-db for reading by esp32
             message: templateMessage,
-            scrollSpeed: parseInt(templateScrollSpeed),
-            intensity: parseInt(templateIntensity),
+            scrollSpeed: speeds[templateScrollSpeed],
+            intensity: intensities[templateIntensity],
             isBeingDisplayed: false 
         });
     });
